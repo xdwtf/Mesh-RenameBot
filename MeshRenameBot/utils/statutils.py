82ab1ebdc07529a44c9f7a2f718a0383a7bdd18f
dishapatel010 <71930916+dishapatel010@users.noreply.github.com>
2021-10-22 15:28:31 +0530

    Update statutils.py

--- MeshRenameBot/utils/statutils.py
+++ MeshRenameBot/utils/statutils.py
@@ -9,6 +9,18 @@ from pyrogram import filters
 LOGGER = logging.getLogger(__name__)
 SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
 
+def get_readable_file_size(size_in_bytes) -> str:
+    if size_in_bytes is None:
+        return '0B'
+    index = 0
+    while size_in_bytes >= 1024:
+        size_in_bytes /= 1024
+        index += 1
+    try:
+        return f'{round(size_in_bytes, 2)}{SIZE_UNITS[index]}'
+    except IndexError:
+        return 'File too large'
+
 def get_readable_time(seconds: int) -> str:
     result = ''
     (days, remainder) = divmod(seconds, 86400)
